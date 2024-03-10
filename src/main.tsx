f2105df44422b447713aeb6d5053be0574aeef8d
Gerardo <jerrytiapalmiotto@gmail.com>
2024-03-10 15:05:16 +0100

    config: i18n configuration i18n: add translations strings

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/App.tsx'
 import './css/index.css'
+import i18n from 'i18next';
+import {initReactI18next} from 'react-i18next';
+import it from './assets/lang/it.json';
+import en from './assets/lang/en.json';
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        translation: en,
+      },
+      it: {
+        translation: it,
+      },
+    },
+    lng: 'en',
+    fallbackLng: 'en',
+  });
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
